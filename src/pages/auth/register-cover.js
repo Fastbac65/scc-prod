182a592fc3cf0d1b3a9629f830d4753d5feac72b
fastbac <fastbac65.gmail.com>
2023-07-17 17:14:45 +1000

    x

--- src/pages/auth/register-cover.js
+++ src/pages/auth/register-cover.js
@@ -1,11 +1,32 @@
 // next
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 // sections
 import { RegisterCoverView } from 'src/sections/auth/view';
+import { useSettingsContext } from 'src/components/settings';
+import LoadingScreen from 'src/components/loading-screen/LoadingScreen';
+import { useEffect } from 'react';
 
 // ----------------------------------------------------------------------
 
 export default function RegisterCoverPage() {
+  const { loading, user, holdRouter } = useSettingsContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && user && !holdRouter) {
+      router.back();
+    }
+  }, [user, loading, holdRouter]);
+
+  if (!holdRouter) {
+    if (user || loading) {
+      return <LoadingScreen />;
+    }
+  }
+
+  // !loading !user
   return (
     <>
       <Head>

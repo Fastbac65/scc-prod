1dba295e212fbb47fb617f41e4123147798f8984
fastbac <fastbac65.gmail.com>
2023-07-28 00:13:12 +1000

    x

--- src/sections/posts/RecentPosts.js
+++ src/sections/posts/RecentPosts.js
@@ -1,10 +1,12 @@
-import { Typography, alpha, styled } from '@mui/material';
+import { Box, Fab, Stack, Tooltip, Typography, alpha, styled } from '@mui/material';
 
 import PostsList from './PostsList';
 
 import { bgGradient } from 'src/lib/cssStyles';
 import { useSettingsContext } from 'src/components/settings';
 import { useEffect, useState } from 'react';
+import Iconify from 'src/components/iconify/Iconify';
+import NewPost from './NewPost';
 
 const StyledRoot = styled('div')(({ theme }) => ({
   ...bgGradient({
@@ -16,7 +18,12 @@ const StyledRoot = styled('div')(({ theme }) => ({
 }));
 
 export default function RecentPosts({ staticPosts }) {
-  const { posts } = useSettingsContext();
+  const {
+    user,
+    posts,
+    dispatch,
+    state: { modal },
+  } = useSettingsContext();
   const [allRecent, setAllRecent] = useState(staticPosts);
 
   useEffect(() => {
@@ -29,9 +36,26 @@ export default function RecentPosts({ staticPosts }) {
     }
   }, [posts]);
 
+  const handleCreatePost = () => {
+    dispatch({ type: 'MODAL', payload: { ...modal, open: true, title: 'Create Post', content: <NewPost /> } });
+  };
+
   return (
     <div>
       <StyledRoot>
+        <Box sx={{ pt: 2 }}>
+          {user && (
+            <Stack spacing={1} direction="row" sx={{ justifyContent: 'center' }}>
+              <Tooltip arrow placement="top-start" title="add post" enterDelay={2000}>
+                <Fab variant="extended" size="small" color="secondary" aria-label="edit" onClick={handleCreatePost}>
+                  {/* <Add /> */}
+                  <Iconify icon="mdi:plus" />
+                  Post
+                </Fab>
+              </Tooltip>
+            </Stack>
+          )}
+        </Box>
         <Typography variant="h4" py={3} mx={2}>
           Recent News
         </Typography>

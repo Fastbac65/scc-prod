406edb5380a0245804043773a3f26a11f46722a4
fastbac <fastbac65.gmail.com>
2023-07-27 23:08:15 +1000

    x

--- src/sections/view/HomeView.js
+++ src/sections/view/HomeView.js
@@ -1,26 +1,17 @@
 // ----------------------------------------------------------------------
 import HomeLanding from 'src/sections/home/HomeLanding';
-import { useSettingsContext } from 'src/components/settings';
 import Posts from '../posts/Posts';
 import RecentPosts from '../posts/RecentPosts';
-import { useEffect, useState } from 'react';
+import OurClubLanding from '../ourclub/OurClubLanding';
 // ----------------------------------------------------------------------
 
 export default function HomeView({ staticPosts }) {
   return (
     <>
-      <RecentPosts staticPosts={staticPosts.slice(0, 6)} />
+      {/* <OurClubLanding /> */}
       <HomeLanding />
       <RecentPosts staticPosts={staticPosts.slice(0, 6)} />
       <Posts staticPosts={staticPosts.slice(6)} />
-      {/* <HomeHero />
-      <HomeGuidedMeditation />
-      <HomeBanner />
-      <HomeFamous />
-      <HomeFAQs />
-      <LatestPosts posts={insights} /> */}
-      {/* <LatestPosts posts={insights.slice(0, 4)} /> */}
-      {/* <TestFooter /> */}
     </>
   );
 }

dc2cc785ddb59efe8e5a8484a2887a609d56d771
fastbac <fastbac65.gmail.com>
2023-09-05 00:08:02 +1000

    x

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -14,13 +14,7 @@ const Notification = () => {
     dispatch({ type: 'UPDATE_ALERT', payload: { ...alert, open: false, variant: 'filled' } }); // everytime we close we revert default variant to 'filled'
   };
   return (
-    <Snackbar
-      open={alert?.open}
-      autoHideDuration={alert.duration}
-      onClose={handleClose}
-      anchorOrigin={{ vertical: alert.posn || 'bottom', horizontal: 'center' }}
-      // sx={{ width: { xs: 400, sm: 'auto' } }}
-    >
+    <Snackbar open={alert?.open} autoHideDuration={alert.duration} onClose={handleClose} anchorOrigin={{ vertical: alert.posn || 'bottom', horizontal: 'center' }} sx={{ zIndex: '20000' }}>
       <Alert onClose={handleClose} severity={alert.severity} sx={{ width: '100%' }} variant={alert.variant} elevation={10}>
         <AlertTitle>{alert.severity}</AlertTitle>
         {alert.message}

efe1d652d0dcd6376943fff1adb7c88ff5249212
fastbac <fastbac65.gmail.com>
2023-09-14 19:18:44 +1000

    x

--- src/sections/posts/PostsList.js
+++ src/sections/posts/PostsList.js
@@ -1,4 +1,4 @@
-import { Box, Fade, Stack } from '@mui/material';
+import { Box, Fade, Stack, Grid2 as Grid } from '@mui/material';
 // import Options from './Options';
 import PostExpandCard from './PostExpandCard';
 import { Masonry } from '@mui/lab';
@@ -14,30 +14,50 @@ const PostsList = ({ posts }) => {
 
   if (!posts) return;
 
+  const odd = posts.filter((post, indx) => {
+    if (indx % 2) return false;
+    return true;
+  });
+  console.log(odd, posts.length);
+
   return (
     <Box component="section" sx={{ mx: 0 }}>
       <Fade timeout={750} in={true}>
         <Box>
-          {/* <Box columnGap={1} display="grid" gridTemplateColumns={{ xs: '1fr', sm: '1fr 1fr' }}>
+          <Box sx={{ p: 1 }} columnGap={1} display="grid" gridTemplateColumns={{ xs: '1fr', sm: '1fr 1fr' }}>
             <Stack>
-              {posts.map((doc, indx) => (
-                <PostExpandCard key={doc.id} user={user} doc={doc} setOpen={setOpen} setCurrentImageIndex={setCurrentImageIndex} setImages={setImages} />
-              ))}
+              {posts
+                .filter((post, indx) => {
+                  if (indx % 2) return false;
+                  return true;
+                })
+                .map((doc, indx) => (
+                  <Box key={doc.id} sx={{ py: 0.5 }}>
+                    <PostExpandCard key={doc.id} user={user} doc={doc} setOpen={setOpen} setCurrentImageIndex={setCurrentImageIndex} setImages={setImages} />
+                  </Box>
+                ))}
             </Stack>
             <Stack>
-              {posts.map((doc, indx) => (
-                <PostExpandCard key={doc.id} user={user} doc={doc} setOpen={setOpen} setCurrentImageIndex={setCurrentImageIndex} setImages={setImages} />
-              ))}
+              {posts
+                .filter((post, indx) => {
+                  if (indx % 2) return true;
+                  return false;
+                })
+                .map((doc, indx) => (
+                  <Box key={doc.id} sx={{ py: 0.5 }}>
+                    <PostExpandCard key={doc.id} user={user} doc={doc} setOpen={setOpen} setCurrentImageIndex={setCurrentImageIndex} setImages={setImages} />
+                  </Box>
+                ))}
             </Stack>
-          </Box> */}
-          <Box pr={{ xs: 0, sm: 2, md: 3 }} sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Masonry /* sx={{ border: '1px dotted red' }} */ columns={{ xs: 1, sm: 2, md: 2, lg: 3 }} spacing={{ xs: 1 }}>
+          </Box>
+          {/* <Box pr={{ xs: 0, sm: 2, md: 3 }} sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Masonry columns={{ xs: 1, sm: 2, md: 2, lg: 3 }} spacing={{ xs: 1 }}>
               {posts.map((doc, indx) => (
                 <PostExpandCard key={doc.id} user={user} doc={doc} setOpen={setOpen} setCurrentImageIndex={setCurrentImageIndex} setImages={setImages} />
               ))}
             </Masonry>
             <PostsLightBox open={open} setOpen={setOpen} currentImageIndex={currentImageIndex} setCurrentImageIndex={setCurrentImageIndex} images={images} />
-          </Box>
+          </Box> */}
         </Box>
       </Fade>
     </Box>

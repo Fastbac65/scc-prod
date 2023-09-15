481b6f599e4a3fbdb08db12c4751e63e2a850eb5
fastbac <fastbac65.gmail.com>
2023-09-16 03:18:56 +1000

    x

--- src/sections/posts/PostOptions.js
+++ src/sections/posts/PostOptions.js
@@ -54,7 +54,7 @@ function PostOptions({ postDoc }) {
 
   return (
     <Box>
-      <Tooltip title="Options" arrow placement="bottom-end">
+      <Tooltip title="Options" arrow placement="top-end">
         <IconButton
           onClick={handleOpenUserMenu}
           aria-label="Options Menu"

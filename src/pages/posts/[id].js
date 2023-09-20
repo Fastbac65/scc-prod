24e4255513ca7c6094f2a6e9219d71aabf5a2e82
fastbac <fastbac65.gmail.com>
2023-09-20 17:37:42 +1000

    x

--- src/pages/posts/[id].js
+++ src/pages/posts/[id].js
@@ -25,7 +25,7 @@ export async function getStaticProps(context) {
       staticPost: post.length ? posts.filter((post) => post.id === context.params.id)[0] : {},
       // title: post.data?.title,
       description: 'South Curl Curl Surf Life Saving Club shared post',
-      canonical: `https:southcurlcurlslsc.com.au/posts/${post.id}`,
+      // canonical: `https:southcurlcurlslsc.com.au/posts/${post.id}`,
     },
     // revalidate: 10,
   };

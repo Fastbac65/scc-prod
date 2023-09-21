ddc04b417f16c3efaab4bdebbeadcf9113f8be6d
fastbac <fastbac65.gmail.com>
2023-09-21 21:30:05 +1000

    sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const config = {
+  siteUrl: 'https://southcurlcurlslsc.com.au',
+  generateRobotsTxt: true, // (Optional parameter for creating robots.txt file)
+  // Other available options..
+};
+
+module.exports = config;

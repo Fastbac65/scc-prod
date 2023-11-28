b65042720675ebd96d5aa9d3319cc308f2e1348c
fastbac <fastbac65.gmail.com>
2023-11-28 16:36:01 +1100

    venue hire payments

--- src/sections/venuehire/VenueHireDetails.js
+++ src/sections/venuehire/VenueHireDetails.js
@@ -1,4 +1,4 @@
-import { Box, Typography, Stack, styled, alpha } from '@mui/material';
+import { Box, Typography, Stack, styled, alpha, Link, Button } from '@mui/material';
 // components
 import Iconify from 'src/components/iconify/Iconify';
 import { bgGradient } from 'src/lib/cssStyles';
@@ -48,28 +48,36 @@ const StyledRootResponsive = styled('div')(({ theme }) => ({
 const VenueHireDetails = () => {
   return (
     <StyledRootResponsive>
-      <Box sx={{ px: 1, pb: 2 }} columnGap={0} display="grid" gridTemplateColumns={{ xs: '1fr', sm: '2fr 3fr', md: 'repeat(2, 1fr)' }}>
-        <Stack></Stack>
-        <Stack>
-          <Typography variant="h3" component="h2" sx={{ mb: 3 }}>
-            Venue Details
-          </Typography>
-          <Stack spacing={1}>
-            <OverviewItem icon="carbon:bar" label="Stylish Wooden Bar" text="Beautiful tables and stools. BYO drinks for you and your guests. Bar service also available." />
-            <OverviewItem icon="material-symbols:soup-kitchen" label="Large Commercial Grade Kitchen" text="Available for all your entertaining requirements. " />
-            <OverviewItem
-              icon="carbon:music"
-              label="State Of The Art Audio/Video Facilities"
-              text="For your music, movies, presentations and conferencing. Everything you could possibly need and easy to setup."
-            />
-            <OverviewItem
-              icon="material-symbols:restaurant"
-              label="Large Bar Servery Area"
-              text="Large private outdoor balcony adjoining the hall. Capacity for up to 120 people. Inside and outside dining with BBQs. "
-            />
+      <Stack spacing={4} display="flex" alignItems="center">
+        <Box sx={{ px: 1, pb: 2 }} columnGap={0} display="grid" gridTemplateColumns={{ xs: '1fr', sm: '2fr 3fr', md: 'repeat(2, 1fr)' }}>
+          <Stack></Stack>
+          <Stack>
+            <Typography variant="h3" component="h2" sx={{ mb: 3 }}>
+              Venue Details
+            </Typography>
+            <Stack spacing={1}>
+              <OverviewItem icon="carbon:bar" label="Stylish Wooden Bar" text="Beautiful tables and stools. BYO drinks for you and your guests. Bar service also available." />
+              <OverviewItem icon="material-symbols:soup-kitchen" label="Large Commercial Grade Kitchen" text="Available for all your entertaining requirements. " />
+              <OverviewItem
+                icon="carbon:music"
+                label="State Of The Art Audio/Video Facilities"
+                text="For your music, movies, presentations and conferencing. Everything you could possibly need and easy to setup."
+              />
+              <OverviewItem
+                icon="material-symbols:restaurant"
+                label="Large Bar Servery Area"
+                text="Large private outdoor balcony adjoining the hall. Capacity for up to 120 people. Inside and outside dining with BBQs. "
+              />
+              <OverviewItem icon="mdi:currency-usd" label="Venue Hire Payments" text="For venue hire payments please visit the link below, select 'South Curl Curl' as entity and transaction type 'Function'." />
+            </Stack>
           </Stack>
-        </Stack>
-      </Box>
+        </Box>
+        <Link target="_blank" rel="noopener" href="https://pnpnet.qvalent.com/OnlinePaymentServlet?cd_community=SLSA&cd_currency=AUD">
+          <Button color="primary" variant="contained" endIcon={<Iconify icon="carbon:launch" />}>
+            Venue Hire Payments
+          </Button>
+        </Link>
+      </Stack>
     </StyledRootResponsive>
   );
 };

be960387e1abfa5b6e0f1c2d183691d3f2c015d9
fastbac <fastbac65.gmail.com>
2023-11-28 17:54:45 +1100

    all menus visible

--- src/layouts/main/nav/config-navigation.js
+++ src/layouts/main/nav/config-navigation.js
@@ -8,6 +8,6 @@ export const navConfig = [
   { access: 'public', title: 'Venue Hire', path: '/venuehire', icon: <Iconify icon="material-symbols:celebration" /> },
   { access: 'public', title: 'Nippers', path: '/nippers', icon: <Iconify icon="mdi:umbrella-beach" /> },
   { access: 'public', title: 'Competitors', path: '/competitors', icon: <Iconify icon="mdi:gym" /> },
-  { access: 'member', title: 'Patrol', path: '/patrol', icon: <Iconify icon="mdi:flag-variant-outline" /> },
-  { access: 'member', title: 'Training', path: '/training', icon: <Iconify icon="icon-park-outline:degree-hat" /> },
+  { access: 'public', title: 'Patrol', path: '/patrol', icon: <Iconify icon="mdi:flag-variant-outline" /> },
+  { access: 'public', title: 'Training', path: '/training', icon: <Iconify icon="icon-park-outline:degree-hat" /> },
 ];

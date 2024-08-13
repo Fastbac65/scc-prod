bca83295b3e2ec83801af20f6086dce24827fe56
fastbac <fastbac65.gmail.com>
2024-08-13 19:13:47 +1000

    x

--- src/sections/venuehire/CalendarVenueHire.js
+++ src/sections/venuehire/CalendarVenueHire.js
@@ -104,14 +104,12 @@ const CalendarVenueHire2 = ({ holidays, booking, social }) => {
     if (!booking) {
       allEvents.current.forEach((event) => {
         if (event.borderColor === `${theme.palette.warning.main}`) {
-          // if (event?.extendedProps?.creator?.email === 'scccaretaker@gmail.com') {
           event.setProp('display', 'none');
         }
       });
     } else if (booking) {
       allEvents.current.forEach((event) => {
         if (event.borderColor === `${theme.palette.warning.main}`) {
-          // if (event?.extendedProps?.creator?.email === 'scccaretaker@gmail.com') {
           event.setProp('display', 'auto');
         }
       });

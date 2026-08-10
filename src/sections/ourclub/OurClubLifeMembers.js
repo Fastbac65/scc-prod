9e0ceef49840192fa1c9614c23ad9ad8b4b2c836
fastbac <fastbac65.gmail.com>
2026-08-10 22:46:40 +1000

    updated life members

--- src/sections/ourclub/OurClubLifeMembers.js
+++ src/sections/ourclub/OurClubLifeMembers.js
@@ -45,116 +45,116 @@ const ourLifeMembers = [
 <tr>
 <td>Barlow, Rodney</td>
 <td>Beavon, Jono</td>
-<td>Blake, C.E.</td>
+<td>Berry, Michael</td>
 </tr>
 <tr>
+<td>Blake, C.E.</td>
 <td>Bland*, Jim</td>
 <td>Brown, Andy</td>
-<td>Brown*, Len A.</td>
 </tr>
 <tr>
+<td>Brown*, Len A.</td>
 <td>Brown, Mark E.</td>
 <td>Brown, Roper ‘Buster’ L. S.</td>
-<td>Buckingham*, Bruce</td>
 </tr>
 <tr>
+<td>Buckingham*, Bruce</td>
 <td>Buckingham, Robert (Bob)</td>
 <td>Cassidy, John ‘Casso’</td>
-<td>Cuneo, Anthony</td>
 </tr>
 <tr>
+<td>Cuneo, Anthony</td>
 <td>Dominish, Graeme</td>
 <td>Dossetor, Graeme</td>
-<td>Drury, Jenny</td>
 </tr>
 <tr>
+<td>Drury, Jenny</td>
 <td>Flemming, Christopher ‘Ribs’</td>
 <td>Flemming,&nbsp;Lyndall</td>
-<td>Flower*, Campbell</td>
 </tr>
 <tr>
+<td>Flower*, Campbell</td>
 <td>Gloag, Mick</td>
 <td>Good, William (Billy)</td>
-<td>Harbron*,&nbsp;Kenneth</td>
 </tr>
 <tr>
+<td>Harbron*,&nbsp;Kenneth</td>
 <td>Harradine, Kevin</td>
 <td>Hewitt, Jeffrey</td>
-<td>Hogan*, Mick</td>
 </tr>
 <tr>
+<td>Hogan*, Mick</td>
 <td>Hourigan*, Cecil A.</td>
 <td>Jones, Darren</td>
-<td>Jones*, Mervyn H.</td>
 </tr>
 <tr>
+<td>Jones*, Mervyn H.</td>
 <td>Joyce*, Denzil [O.A.M.]</td>
 <td>Kuhn, Charles ‘Chicka’</td>
-<td>Leggett*, Charles</td>
 </tr>
 <tr>
+<td>Leggett*, Charles</td>
 <td>Longley, Charles</td>
 <td>Lowery,&nbsp;Michelle</td>
-<td>Lowery, Robert</td>
 </tr>
 <tr>
+<td>Lowery, Robert</td>
 <td>Lowther, Peter</td>
 <td>MacPherson*, Robert (Bob) G.</td>
-<td>MacPherson*, William (Bill) J.C.</td>
 </tr>
 <tr>
+<td>MacPherson*, William (Bill) J.C.</td>
 <td>Marvin, Ron L.</td>
 <td>McColm*, John G.</td>
-<td>McErlane*, David</td>
 </tr>
 <tr>
+<td>McErlane*, David</td>
 <td>McKay, Elton</td>
 <td>Middleton*, Tom A.</td>
-<td>Moffat, Kevin ‘Spook’ A.</td>
 </tr>
 <tr>
+<td>Moffat, Kevin ‘Spook’ A.</td>
 <td>Mulvey, Anthony ‘Champ’</td>
 <td>Newman*, Sam H.</td>
-<td>O’Keefe*, Lindsay</td>
 </tr>
 <tr>
+<td>O’Keefe*, Lindsay</td>
 <td>Olivey*, David</td>
 <td>Olsen*, Arthur O.</td>
-<td>Olsen*, Clive E.</td>
 </tr>
 <tr>
+<td>Olsen*, Clive E.</td>
 <td>Picard*, Robert (Bob)</td>
 <td>Raymond*, Ernie</td>
-<td>Rees, Raymond ‘Raz’</td>
 </tr>
 <tr>
+<td>Rees, Raymond ‘Raz’</td>
 <td>Roberts*, A. ‘Ritchie’</td>
 <td>Routh*, Jim</td>
-<td>Russell, Bryn</td>
 </tr>
 <tr>
+<td>Russell, Bryn</td>
 <td>Russell, Janice</td>
 <td>Settree, Lisa</td>
-<td>Settree, Wayne ‘Zulu’</td>
 </tr>
 <tr>
+<td>Settree, Wayne ‘Zulu’</td>
 <td>Skelton, Jordi</td>
 <td>Spiteri, Mark ‘Angus’</td>
-<td>Stacey*, Tom</td>
 </tr>
 <tr>
+<td>Stacey*, Tom</td>
 <td>Stephens*, G. (Reg)</td>
 <td>Still*, Arthur</td>
-<td>Stoker, Darrin</td>
 </tr>
 <tr>
+<td>Stoker, Darrin</td>
 <td>Taylor*, Norm R.</td>
 <td>Thompson, Martin</td>
-<td>White, John</td>
 </tr>
 <tr>
+<td>White, John</td>
 <td>Woodward, Michael</td>
-<td></td>
 <td></td>
 </tr>
 <tr>
